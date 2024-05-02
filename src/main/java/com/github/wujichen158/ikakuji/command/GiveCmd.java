5d1afe16904cbc27ab9e3c0a691d65632bee62b0
wujichen158 <1169083089@qq.com>
2024-05-02 15:50:40 +0800

    fix(give cmd): Fix an issue that give cmd only check item names

--- src/main/java/com/github/wujichen158/ikakuji/command/GiveCmd.java
+++ src/main/java/com/github/wujichen158/ikakuji/command/GiveCmd.java
@@ -6,22 +6,29 @@ import com.envyful.api.command.annotate.executor.CommandProcessor;
 import com.envyful.api.command.annotate.executor.Completable;
 import com.envyful.api.command.annotate.executor.Sender;
 import com.envyful.api.command.annotate.permission.Permissible;
+import com.envyful.api.config.type.ExtendedConfigItem;
 import com.envyful.api.forge.command.completion.player.PlayerTabCompleter;
 import com.envyful.api.forge.config.UtilConfigItem;
 import com.github.wujichen158.ikakuji.IkaKuji;
 import com.github.wujichen158.ikakuji.command.completion.CrateDeliverCompleter;
 import com.github.wujichen158.ikakuji.command.completion.CrateNameCompleter;
 import com.github.wujichen158.ikakuji.config.IkaKujiLocaleCfg;
+import com.github.wujichen158.ikakuji.config.KujiCrateType;
 import com.github.wujichen158.ikakuji.kuji.EnumCrateType;
 import com.github.wujichen158.ikakuji.lib.PermissionNodes;
 import com.github.wujichen158.ikakuji.util.CrateFactory;
 import com.github.wujichen158.ikakuji.util.MsgUtil;
+import com.google.common.collect.BiMap;
 import com.google.common.collect.Lists;
 import net.minecraft.command.ICommandSource;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.spongepowered.configurate.ConfigurateException;
+import org.spongepowered.configurate.ConfigurationNode;
+import org.spongepowered.configurate.serialize.SerializationException;
+import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
 
 import java.util.List;
 import java.util.Map;
@@ -39,29 +46,33 @@ public class GiveCmd {
                     @Completable(PlayerTabCompleter.class) @Argument ServerPlayerEntity targetPlayer,
                     @Completable(CrateNameCompleter.class) @Argument String crateName) {
         Optional.ofNullable(CrateFactory.get(crateName)).ifPresent(crate -> {
-            List<ItemStack> itemStacks = Lists.newArrayList();
+            List<ItemStack> itemStacks = null;
             IkaKujiLocaleCfg.Commands commands = IkaKuji.getInstance().getLocale().getCommands();
             if (CrateDeliverCompleter.KEY.equalsIgnoreCase(type)) {
-                Optional.ofNullable(crate.getKey()).ifPresentOrElse(key -> {
+                ExtendedConfigItem key = crate.getKey();
+                if (Optional.ofNullable(key).isPresent()) {
+                    itemStacks = Lists.newArrayList();
                     itemStacks.add(UtilConfigItem.fromConfigItem(key));
                     sender.sendMessage(MsgUtil.prefixedColorMsg(commands.getGiveKey(), crateName, targetPlayer.getName().getString()), targetPlayer.getUUID());
-                }, () -> sender.sendMessage(MsgUtil.prefixedColorMsg(commands.getNoKey(), crateName), targetPlayer.getUUID()));
+                } else {
+                    sender.sendMessage(MsgUtil.prefixedColorMsg(commands.getNoKey(), crateName), targetPlayer.getUUID());
+                }
             } else if (CrateDeliverCompleter.CRATE.equalsIgnoreCase(type)) {
                 if (crate.getCrateType() == EnumCrateType.item && CrateFactory.getAll().containsKey(crateName)) {
-                    for (Map<String, String> typeDatum : crate.getTypeData()) {
-                        String itemName = typeDatum.get(type);
-                        if (Optional.ofNullable(itemName).isPresent()) {
-                            itemStacks.add(new ItemStack(ForgeRegistries.ITEMS.getValue(ResourceLocation.tryParse(itemName))));
-                            sender.sendMessage(MsgUtil.prefixedColorMsg(commands.getGiveCrate(), crateName, targetPlayer.getName().getString()), targetPlayer.getUUID());
-                        } else {
-                            sender.sendMessage(MsgUtil.prefixedColorMsg(commands.getInvalidItemCrate(), crateName), targetPlayer.getUUID());
-                        }
+                    List<ItemStack> itemStacks1 = CrateFactory.getItemsFromName(crateName);
+                    if (Optional.ofNullable(itemStacks1).isPresent()) {
+                        itemStacks = itemStacks1;
+                        sender.sendMessage(MsgUtil.prefixedColorMsg(commands.getGiveCrate(), crateName, targetPlayer.getName().getString()), targetPlayer.getUUID());
+                    } else {
+                        sender.sendMessage(MsgUtil.prefixedColorMsg(commands.getInvalidItemCrate(), crateName), targetPlayer.getUUID());
                     }
                 } else {
                     sender.sendMessage(MsgUtil.prefixedColorMsg(commands.getNotItemCrate(), crateName), targetPlayer.getUUID());
                 }
             }
-            itemStacks.forEach(targetPlayer::addItem);
+
+            // Give
+            Optional.ofNullable(itemStacks).ifPresent(itemStacksNonNull -> itemStacksNonNull.forEach(targetPlayer::addItem));
         });
     }
 }
